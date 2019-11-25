e289a8dae6c64812aa26dd71930bea372a9c12fd
Daniel Childs <daniel.alexander.edward.childs@everis.com>
2019-11-25 14:08:02 +0000

    Revert "Add player api that will be extened later on"
    This reverts commit 9da570abf126c52bc9217cc486a77d0645f5ffe1.

--- src/main/java/com/nailed94/DAO/PlayerDao.java
+++
@@ -1,9 +0,0 @@
-package com.nailed94.DAO;
-
-import com.nailed94.DTO.Player;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerDao extends CrudRepository<Player, Integer> {
-}
