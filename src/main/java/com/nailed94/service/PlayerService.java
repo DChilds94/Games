9da570abf126c52bc9217cc486a77d0645f5ffe1
Daniel Childs <daniel.alexander.edward.childs@everis.com>
2019-11-25 11:29:13 +0000

    Add player api that will be extened later on

---
+++ src/main/java/com/nailed94/service/PlayerService.java
@@ -0,0 +1,38 @@
+package com.nailed94.service;
+
+import com.nailed94.DAO.PlayerDao;
+import com.nailed94.DTO.Player;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class git PlayerService {
+    @Autowired
+    private PlayerDao playerDao;
+
+    public List<Player> getAllPlayers() {
+        List<Player> players = new ArrayList<>();
+        playerDao.findAll().forEach(players::add);
+        return players;
+    }
+
+    public Player findById(Integer id) {
+        return playerDao.findById(id).orElse(null);
+    }
+
+    public void addPlayer(Player player) {
+        playerDao.save(player);
+    }
+
+    public void updatePlayer(Player player, Integer id) {
+        playerDao.save(player);
+    }
+
+    public void deleteById(Integer id) {
+        playerDao.deleteById(id);
+    }
+
+}
