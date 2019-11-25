03eb947ee9daeaef075a2020092fc4a27471b42e
Daniel Childs <daniel.alexander.edward.childs@everis.com>
2019-11-25 10:47:09 +0000

    Map Game DTO to the database

--- src/main/java/com/nailed94/DTO/Game.java
+++ src/main/java/com/nailed94/DTO/Game.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Game {
     // This class will expand to contain all the properties of the game,
+    @Id
     private Integer id;
     private String name;
 
